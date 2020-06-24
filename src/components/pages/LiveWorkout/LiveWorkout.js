8a9e4cb6ca4d6cf922e478c369c616924adda351
John Fleming <johndeclanfleming@gmail.com>
2020-06-23 20:34:20 -0500

    axios get single workout on component mount

--- src/components/pages/LiveWorkout/LiveWorkout.js
+++ src/components/pages/LiveWorkout/LiveWorkout.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './LiveWorkout.scss';
 
+import workoutData from '../../../helpers/data/workoutData';
+
 class LiveWorkout extends React.Component {
+  state = {
+    workout: {},
+  }
+
+  componentDidMount() {
+    const { workoutId } = this.props.match.params;
+    workoutData.getSingleWorkout(workoutId)
+      .then((resp) => this.setState({ workout: resp.data }))
+      .catch((err) => console.error('could not get specific workout: , err'));
+  }
+
   render() {
     return (
       <div className="LiveWorkout">
--- src/helpers/data/workoutData.js
+++ src/helpers/data/workoutData.js
@@ -3,6 +3,8 @@ import firebaseConfig from '../apiKeys.json';
 
 const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
 
+const getSingleWorkout = (workoutId) => axios.get(`${baseUrl}/workouts/${workoutId}.json`);
+
 const createWorkout = (newWorkout) => axios.post(`${baseUrl}/workouts.json`, newWorkout);
 
-export default { createWorkout };
+export default { createWorkout, getSingleWorkout };
