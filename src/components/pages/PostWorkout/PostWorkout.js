e7bb01bca69f629618bcced5ea1c7c7e9c604055
John Fleming <johndeclanfleming@gmail.com>
2020-06-24 20:57:38 -0500

    set initial state to workout favorited status

--- src/components/pages/PostWorkout/PostWorkout.js
+++ src/components/pages/PostWorkout/PostWorkout.js
@@ -2,13 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './PostWorkout.scss';
 
+import workoutData from '../../../helpers/data/workoutData';
+
 class PostWorkout extends React.Component {
+  state = {
+    isChecked: '',
+  }
+
+  componentDidMount() {
+    const { workoutId } = this.props.match.params;
+    workoutData.getSingleWorkout(workoutId)
+      .then((resp) => {
+        const isChecked = resp.data.isFavorited;
+        this.setState({ isChecked });
+      })
+      .catch((err) => console.error('could not get specific workout: , err'));
+  }
+
+  checkboxChange = (e) => {
+    e.preventDefault();
+    this.setState({ isChecked: !this.state.isChecked });
+  }
+
   render() {
+    const { isChecked } = this.state;
     return (
       <div className="PostWorkout">
-        <h2>PostWorkout</h2>
-        <Link className="btn btn-outline-dark" to='/home'>Skip to Home</Link>
-        <Link className="btn btn-outline-dark" to='/profile'>To Profile</Link>
+        <h2>You did it!</h2>
+        <h5 className="mt-5">Loved this workout?</h5>
+        <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="favorites-checkbox" value={isChecked} onChange={this.checkboxChange}/>
+          <label className="form-check-label" htmlFor="favorites-checkbox">Add to your favorites</label>
+        </div>
+        <div className="feedback-btns">
+          <Link className="btn btn-outline-dark" to='/home'>Skip to Home</Link>
+          <Link className="btn btn-outline-dark" to='/profile'>To Profile</Link>
+        </div>
       </div>
     );
   }
