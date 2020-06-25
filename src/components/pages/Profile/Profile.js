27f738c0ce9fce7fc9926e5bff140ca14394423f
John Fleming <johndeclanfleming@gmail.com>
2020-06-25 17:04:49 -0500

    wip

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -2,13 +2,69 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Profile.scss';
 
+import authData from '../../../helpers/data/authData';
+import completedWorkoutData from '../../../helpers/data/completedWorkoutData';
+import workoutData from '../../../helpers/data/workoutData';
+
 class Profile extends React.Component {
+  state = {
+    username: '',
+    userAvatar: '',
+    completedWorkouts: [],
+    favoriteWorkouts: [],
+  }
+
+  getAuthedUser() {
+    const username = authData.getCurrentUser().displayName;
+    const userAvatar = authData.getCurrentUser().photoURL;
+    this.setState({ username, userAvatar });
+  }
+
+  getCompletedWorkouts() {
+    const uid = authData.getUid();
+    completedWorkoutData.getCompletedWorkoutsByUid(uid)
+      .then((resp) => this.setState({ completedWorkouts: resp }))
+      .catch((err) => console.error('could not get completed workouts: ', err));
+  }
+
+  getFavoriteWorkouts() {
+    const uid = authData.getUid();
+    workoutData.getFavoriteWorkoutsByUid(uid)
+      .then((resp) => this.setState({ favoriteWorkouts: resp }))
+      .catch((err) => console.error('could not get favorite workouts: ', err));
+  }
+
+  componentDidMount() {
+    this.getAuthedUser();
+    this.getCompletedWorkouts();
+    this.getFavoriteWorkouts();
+  }
+
   render() {
+    const { username, userAvatar, completedWorkouts } = this.state;
     return (
       <div className="Profile">
-        <h2>Profile</h2>
-        <Link className="btn btn-outline-dark" to='/home'>Go to to Home</Link>
-        <Link className="btn btn-outline-dark" to='/favorites'>To Favorites</Link>
+        <div className="profile-card row col-10 offset-1">
+          <div className="col-4 profile-card-left">
+            <img src={userAvatar} alt=""/>
+          </div>
+          <div className="col-8 profile-card-right text-center">
+            <h4>{username}</h4>
+            <div className="row workout-counts">
+              <div className="col-6 completed-workout-container">
+                <h6 className="workout-counts-header">Completed</h6>
+                <p className="completed-workout-count">{completedWorkouts.length}</p>
+              </div>
+              <div className="col-6 favorited-workout-container">
+              <h6 className="workout-counts-header">Favorited</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="profile-btns d-flex flex-column col-10 offset-1">
+          <Link className="btn btn-outline-dark mt-4" to='/home'>Start New Workout</Link>
+          <Link className="btn btn-outline-dark mt-3" to='/favorites'>View Favorites</Link>
+        </div>
       </div>
     );
   }
--- src/helpers/data/workoutData.js
+++ src/helpers/data/workoutData.js
@@ -3,6 +3,22 @@ import firebaseConfig from '../apiKeys.json';
 
 const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
 
+const getFavoriteWorkoutsByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/workouts.json?orderBy="UID"&equalTo="${uid}"`)
+    .then((resp) => {
+      const allWorkouts = resp.data;
+      const workouts = [];
+      if (allWorkouts != null) {
+        Object.keys(allWorkouts).forEach((wId) => {
+          allWorkouts[wId].completeId = wId;
+          workouts.push(allWorkouts[wId]);
+        });
+      }
+      resolve(workouts);
+    })
+    .catch((err) => reject(err));
+});
+
 const getSingleWorkout = (workoutId) => axios.get(`${baseUrl}/workouts/${workoutId}.json`);
 
 const createWorkout = (newWorkout) => axios.post(`${baseUrl}/workouts.json`, newWorkout);
@@ -12,8 +28,9 @@ const deleteSingleWorkout = (workoutId) => axios.delete(`${baseUrl}/workouts/${w
 const updateFavoritedStatus = (workoutId, isFavorited) => axios.patch(`${baseUrl}/workouts/${workoutId}.json`, { isFavorited });
 
 export default {
-  createWorkout,
+  getFavoriteWorkoutsByUid,
   getSingleWorkout,
+  createWorkout,
   deleteSingleWorkout,
   updateFavoritedStatus,
 };
