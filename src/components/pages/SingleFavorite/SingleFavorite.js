c802c43c495d2c12a7fa889078aa5a7c782dfb9c
John Fleming <johndeclanfleming@gmail.com>
2020-06-26 17:56:13 -0500

    wip

--- src/components/pages/SingleFavorite/SingleFavorite.js
+++ src/components/pages/SingleFavorite/SingleFavorite.js
@@ -6,8 +6,10 @@ class SingleFavorite extends React.Component {
   render() {
     return (
       <div className="SingleFavorite">
-        <h2>SingleFavorite</h2>
-        <Link className="btn btn-outline-dark" to='/profile'>Back to Profile</Link>
+        <div className="favorites-header row col-10 offset-1">
+          <Link className="btn mr-2 back-btn" to='/profile'><i className="fas fa-chevron-left"></i></Link>
+          <h2>Favorited Workouts</h2>
+        </div>
         <Link className="btn btn-outline-dark" to='/workout/:workoutId'>To Live Workout</Link>
       </div>
     );
