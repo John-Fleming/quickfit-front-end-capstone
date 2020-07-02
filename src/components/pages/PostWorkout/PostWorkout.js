5f495b3f795244a9d00136c30832d275c4dbae94
John Fleming <johndeclanfleming@gmail.com>
2020-07-01 20:06:51 -0500

    added hero image and positioned elements

--- src/components/pages/PostWorkout/PostWorkout.js
+++ src/components/pages/PostWorkout/PostWorkout.js
@@ -36,15 +36,21 @@ class PostWorkout extends React.Component {
     const { isChecked } = this.state;
     return (
       <div className="PostWorkout">
-        <h2>You did it!</h2>
-        <h5 className="mt-5">Loved this workout?</h5>
-        <div className="form-check">
-          <input type="checkbox" className="form-check-input" id="favorites-checkbox" value={isChecked} onChange={this.checkboxChange}/>
-          <label className="form-check-label" htmlFor="favorites-checkbox">Add to your favorites</label>
+        <div className="header-bg">
+          <h2 id="post-workout-header" className="text-center">You did it!</h2>
         </div>
-        <div className="feedback-btns">
-          <Link className="btn btn-outline-dark" to='/home'>Skip</Link>
-          <button className="btn btn-outline-dark" onClick={this.patchIsFavorited}>Save</button>
+        <div className="feedback-container">
+          <div className="feedback-form">
+            <h5>Loved this workout?</h5>
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" id="favorites-checkbox" value={isChecked} onChange={this.checkboxChange}/>
+              <label className="form-check-label" htmlFor="favorites-checkbox">Add to your favorites</label>
+            </div>
+          </div>
+          <div className="feedback-btn mt-3">
+            <Link className="btn btn-outline-light mr-3" to='/home'>Skip</Link>
+            <button className="btn btn-outline-light" onClick={this.patchIsFavorited}>Save</button>
+          </div>
         </div>
       </div>
     );
